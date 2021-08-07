f4d43affadebd527c3fe8e5bdf2f8c0ff1bb8def
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-08-07 13:23:38 +0200

    Displayed heart instead of ellipse drop

--- drop.js
+++ drop.js
@@ -5,24 +5,25 @@ function Drop(x, y) {
     this.r = 8;
 
     this.show = function() {
-        noStroke();
-        fill(150, 0, 255);
-        ellipse(this.x, this.y, this.r*2, this.r*2);
+        // noStroke();
+        // fill(150, 0, 255);
+        // ellipse(this.x, this.y, this.r * 2, this.r * 2);
+        image(heartImg, this.x, y - 20, 20, 20);
     }
 
     this.evaporate = function() {
         this.toDelete = true;
-      };
-      
+    };
+
     // r = radius ;-) 
     this.hits = function(flower) {
         var d = dist(this.x, this.y, flower.x, flower.y);
         if (d < this.r + flower.r) {
-          return true;
+            return true;
         } else {
-          return false;
+            return false;
         }
-      };
+    };
 
     this.move = function() {
         this.y = this.y - 5; // Spacing for the bullets being shot.
