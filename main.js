58c34588563db2072b7f4c974b28ec7c01e6120e
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-08-07 12:05:56 +0200

    Centered the canvas

--- main.js
+++ main.js
@@ -1,56 +1,59 @@
 // Sets up the program (J5)
+var cnv;
 var ship;
 var flowers = [];
 var drops = [];
 
 function setup() {
-  createCanvas(600, 400);
-  ship = new Ship();
-  // drop = new Drop(width/2, height/2);
-  for (var i = 0; i < 6; i++) {
-    flowers[i] = new Flower(i * 80 + 80, 60);
-  }
+    cnv = createCanvas(600, 400);
+    cnv.parent('canvas');
+
+    ship = new Ship();
+    // drop = new Drop(width/2, height/2);
+    for (var i = 0; i < 6; i++) {
+        flowers[i] = new Flower(i * 80 + 80, 60);
+    }
 }
 
 function draw() {
-  background(51);
-  ship.show();
-  ship.move();
+    background(51);
+    ship.show();
+    ship.move();
 
-  for (var i = 0; i < drops.length; i++) {
-    drops[i].show();
-    drops[i].move();
-    for (var j = 0; j < flowers.length; j++) {
-      if (drops[i].hits(flowers[j])) {
-        flowers[j].grow();
-        drops[i].evaporate();
-      }
+    for (var i = 0; i < drops.length; i++) {
+        drops[i].show();
+        drops[i].move();
+        for (var j = 0; j < flowers.length; j++) {
+            if (drops[i].hits(flowers[j])) {
+                flowers[j].grow();
+                drops[i].evaporate();
+            }
+        }
     }
-  }
 
-  var edge = false;
+    var edge = false;
 
-  // Edge detection
+    // Edge detection
 
-  for (var i = 0; i < flowers.length; i++) {
-    flowers[i].show();
-    flowers[i].move();
-    if (flowers[i].x > width || flowers[i].x < 0) {
-      edge = true;
-    }
-  }
-
-  if (edge) {
     for (var i = 0; i < flowers.length; i++) {
-      flowers[i].shiftDown();
+        flowers[i].show();
+        flowers[i].move();
+        if (flowers[i].x > width || flowers[i].x < 0) {
+            edge = true;
+        }
     }
-  }
 
-  for (var i = drops.length - 1; i >= 0; i--) {
-    if (drops[i].toDelete) {
-      drops.splice(i, 1);
+    if (edge) {
+        for (var i = 0; i < flowers.length; i++) {
+            flowers[i].shiftDown();
+        }
     }
-  }
+
+    for (var i = drops.length - 1; i >= 0; i--) {
+        if (drops[i].toDelete) {
+            drops.splice(i, 1);
+        }
+    }
 }
 // Function for key releasing:
 function keyReleased() {
