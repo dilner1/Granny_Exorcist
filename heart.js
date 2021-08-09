64498569048250564acf3eac67f1a7b3ec307c39
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-08-09 11:14:42 +0200

    Renamed: flower(s) to monster(s), drop(s) to heart(s)

---
+++ heart.js
@@ -0,0 +1,43 @@
+// This is for the bullets (Called drops for the moment) etc whatever we want to shoot
+
+
+function Heart(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.r = 8;
+
+    this.show = function() {
+        noStroke();
+        fill(255, 0, 0);
+        // ellipse(this.x, this.y, this.r * 1, this.r * 2);
+        heartShape(this.x, this.y, 20);
+        //image(heartImg, this.x, y - 20, 20, 20);
+    }
+
+    this.evaporate = function() {
+        this.toDelete = true;
+    };
+
+    // r = radius ;-) 
+    this.hits = function(flower) {
+        var d = dist(this.x, this.y, flower.x, flower.y);
+        if (d < this.r + flower.r) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.move = function() {
+        this.y = this.y - 3.5; // Spacing for the bullets being shot.
+    }
+}
+
+// this function creates Custom shapes (heart)
+function heartShape(x, y, size) {
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+    endShape(CLOSE);
+}
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var grannyImg;
 var heartImg;
 var monsterImg;
 var monsterImgHit;
-var flowers = [];
+var monsters = [];
 var hearts = [];
 
 function preload() {
@@ -21,7 +21,7 @@ function setup() {
     granny = new Granny();
     // drop = new Drop(width/2, height/2);
     for (var i = 0; i < 6; i++) {
-        flowers[i] = new Flower(i * 80 + 80, 60);
+        monsters[i] = new Monster(i * 80 + 80, 60);
     }
 }
 
@@ -33,20 +33,20 @@ function draw() {
     for (var i = 0; i < hearts.length; i++) {
         hearts[i].show();
         hearts[i].move();
-        for (var j = 0; j < flowers.length; j++) {
-            if (hearts[i].hits(flowers[j])) {
-                //flowers[j].grow();
+        for (var j = 0; j < monsters.length; j++) {
+            if (hearts[i].hits(monsters[j])) {
+                //monsters[j].grow();
                 // calculating the score of granny and removing the dead monster
-                let score = flowers[j].change(); 
-                flowers.splice(j, 1)
-                if (score === 6){
+                let score = monsters[j].change();
+                monsters.splice(j, 1)
+                if (score === 6) {
                     // alert(score);
                     fill(255);
                     textSize(40);
                     textAlign(CENTER, CENTER)
-                    text("Granny Wins!", width/2, height/2);
-                } 
-                
+                    text("Granny Wins!", width / 2, height / 2);
+                }
+
                 hearts[i].evaporate();
             }
         }
@@ -56,17 +56,17 @@ function draw() {
 
     // Edge detection
 
-    for (var i = 0; i < flowers.length; i++) {
-        flowers[i].show();
-        flowers[i].move();
-        if (flowers[i].x > width || flowers[i].x < 0) {
+    for (var i = 0; i < monsters.length; i++) {
+        monsters[i].show();
+        monsters[i].move();
+        if (monsters[i].x > width || monsters[i].x < 0) {
             edge = true;
         }
     }
 
     if (edge) {
-        for (var i = 0; i < flowers.length; i++) {
-            flowers[i].shiftDown();
+        for (var i = 0; i < monsters.length; i++) {
+            monsters[i].shiftDown();
         }
     }
 
@@ -90,7 +90,7 @@ function keyPressed() {
         var heart = new Heart(granny.x, height);
         hearts.push(heart);
     }
-    
+
     if (keyCode === RIGHT_ARROW) {
         granny.setDir(1);
     } else if (keyCode === LEFT_ARROW) {
