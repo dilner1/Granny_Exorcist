cbe55ad1175ef0cbe254529fe902717dbfc328c3
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-08-09 11:04:59 +0200

    Increased monster speed after coming down a row

--- flower.js
+++ flower.js
@@ -1,5 +1,6 @@
 // These are our aliens (called flowers here) etc whatever we want to attack
 var score = 0;
+
 function Flower(x, y) {
     this.x = x;
     this.y = y;
@@ -15,24 +16,24 @@ function Flower(x, y) {
     // this is what happens when the heart hits a monster
     this.change = function() {
         //image(heartImg, this.x, this.y, 50, 50);
-        return(++score);
+        return (++score);
     }
 
     this.shiftDown = function() {
-        this.xdir *= -1;
+        this.xdir *= -1.25;
         this.y += this.r;
     }
 
     this.move = function() {
         this.x = this.x + this.xdir;
     }
-    
+
     this.show = function() {
         noStroke();
-        fill(255, 0, 200, 150); 
+        fill(255, 0, 200, 150);
         rectMode(CENTER);
         //ellipse(this.x, this.y, this.r*2, this.r*2);
-        
+
         image(monsterImg, this.x, this.y, 50, 50)
     }
 }
